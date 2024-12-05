c6aa6444a5789145657c35915857e0a2b5cd28d3
abhishek <abhishakem@hotmail.com>
2024-12-05 19:04:56 +0530

    Added features that includes time checking at intervals and populates respective widgets in UI

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 
-const sampleRecord = {id:-1,reminderNote:"Sample note",remindingTime: getRespectiveDate(),isSent:false};
+var sampleRecord = { id: -1, reminderNote: "Sample note", remindingTime: getRespectiveDate(), isSent: false };
 
-if(localStorage.getItem("localDb")===null){ 
-    var localDb= [  ];
-    localStorage.setItem("localDb",JSON.stringify(localDb));
+if (localStorage.getItem("localDb") === null) {
+    var localDb = [];
+    localStorage.setItem("localDb", JSON.stringify(localDb));
 }
 
 
@@ -13,16 +13,28 @@ function createNewTask() {
     let localDb = localStorage.getItem('localDb');
 
     localDb = JSON.parse(localDb);
-    totalRecords =localDb.length;
+    totalRecords = localDb.length;
 
-    const task = Object.create(sampleRecord)
-    task.id = totalRecords+1;
-    task.reminderNote = newTaskText;
-    task.remindingTime = whenTime;
-    task.isSent=false;
-    localDb.push(task);
-    localStorage.setItem("localDb",JSON.stringify(localDb));
-    alert("Created!")
+
+
+    let capturedTs = new Date(whenTime).getTime();
+    let currentTs = new Date().getTime()
+
+    if (capturedTs > currentTs) {
+        const task = Object.create(sampleRecord)
+        task.id = totalRecords + 1;
+        task.reminderNote = newTaskText;
+        task.remindingTime = whenTime;
+        task.isSent = false;
+        localDb.push(task);
+        localStorage.setItem("localDb", JSON.stringify(localDb));
+        alert("Created!")
+        fillScheduled();
+
+    }
+    else {
+        alert("Can't create a reminder in the past!")
+    }
 
 }
 
@@ -32,10 +44,10 @@ function getRespectiveDate(type) {
     if (type === "today") {
         today.setHours(today.getHours() + 1);
     }
-    else if( type==undefined) {
-          
+    else if (type == undefined) {
+
     }
-    else{
+    else {
         today.setDate(today.getDate() + 1);
     }
     day = today.getDate();
@@ -48,11 +60,11 @@ function getRespectiveDate(type) {
 
 
 
-const todayBtn = document.getElementById("todayBtn");
-const tomBtn = document.getElementById("tomBtn");
-const createBtn = document.getElementById("createBtn");
+var todayBtn = document.getElementById("todayBtn");
+var tomBtn = document.getElementById("tomBtn");
+var createBtn = document.getElementById("createBtn");
 
-
+if( todayBtn!==null) { 
 todayBtn.addEventListener("click", function () {
     document.getElementById("whenTime").value = getRespectiveDate("today");
     document.getElementById("whenTime").select()
@@ -63,22 +75,87 @@ tomBtn.addEventListener("click", function () {
     document.getElementById("whenTime").select()
 });
 
-createBtn.addEventListener("click", function(){
+createBtn.addEventListener("click", function () {
     createNewTask();
 });
+}
+
+function fillScheduled() {
+
+    var tableTarget  = document.getElementsByClassName('sch-records')[0];
+    var preparedRecord  = document.getElementsByClassName('sch-tr')[0];
+    if(tableTarget!==undefined){ 
+    var localDb = localStorage.getItem('localDb');
+
+    localDb =  JSON.parse(localDb);
+    tableTarget.children[1].innerHTML="";
+    localDb.forEach(item => {
+        if(item.isSent ===false ) { 
+        let newRec = preparedRecord.cloneNode(true);
+        newRec.children[0].innerHTML=item.id;
+        newRec.children[1].innerHTML=item.reminderNote;
+        newRec.children[2].innerHTML=new Date(item.remindingTime).toLocaleString();
+        
+        tableTarget.children[1].append(newRec)}
+
+    });
+    }
 
 
-function fillUpcomingTasks(){
+}
+function fillCompletedTasks() {
+    var tableTarget  = document.getElementsByClassName('cmp-records')[0];
+    var preparedRecord  = document.getElementsByClassName('cmp-tr')[0];
+    if(tableTarget!==undefined){ 
+        var localDb = localStorage.getItem('localDb');
+
+        localDb =  JSON.parse(localDb);
+        tableTarget.children[1].innerHTML="";
+    
+        localDb.forEach(item => {
+            if(item.isSent ===true ) { 
+            let newRec = preparedRecord.cloneNode(true);
+            newRec.children[0].innerHTML=item.id;
+            newRec.children[1].innerHTML=item.reminderNote;
+            newRec.children[2].innerHTML=new Date(item.remindingTime).toLocaleString();
+            
+            tableTarget.children[1].append(newRec)}
+    
+        });
+    }
+    
+
+}
+
+
+function sendReminder(){
 
     
 }
-function fillCompletedTasks(){
-let localDb = localStorage.getItem('localDb');
-localDb = []//JSON.parse(localDb);
 
-localDb.forEach(element => {
-    
-});
+function validateTimers(){
+    var localDb = localStorage.getItem('localDb');
+
+    localDb =  JSON.parse(localDb);
 
     
+    const currentTs = new Date().getTime()
+
+
+    localDb.forEach(item => {
+        var capturedTs = new Date(item.remindingTime).getTime();
+        if (capturedTs < currentTs) {
+            item.isSent=true;
+
+        }
+
+
+    });
+
+    localStorage.setItem("localDb", JSON.stringify(localDb));
+   
 }
+
+setInterval(fillCompletedTasks, 1000);
+setInterval(fillScheduled, 1000);
+setInterval(validateTimers, 1000);
