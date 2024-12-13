d2cfc93b37824d182f0972d9c4a259cf622fba6d
abhishek <abhishakem@hotmail.com>
2024-12-14 00:55:18 +0530

    Added functionality for delete reminder

--- js/script.js
+++ js/script.js
@@ -1,20 +1,25 @@
-class localDbClass{
+class localDbClass {
 
-    static load(){
+    static load() {
 
         this.localDb = JSON.parse(localStorage.getItem('localDb'))
         return this.localDb
     }
-    static save(){
+    static save() {
         console.log("Saved localdb")
-        localStorage.setItem("localDb",JSON.stringify(this.localDb))
-           
-       }
-    static showData(){
+        localStorage.setItem("localDb", JSON.stringify(this.localDb))
 
-        this.localDb.forEach(item => {console.log(item)})
     }
-    static markDismissed(id){
+    static clear() {
+        this.localDb = []
+        this.save()
+
+    }
+    static showData() {
+
+        this.localDb.forEach(item => { console.log(item) })
+    }
+    static markDismissed(id) {
         this.localDb.forEach(item => {
 
             if (item.id === id) {
@@ -26,7 +31,7 @@ class localDbClass{
         this.save();
     }
 
-    static unmarkDismissed(id){
+    static unmarkDismissed(id) {
         this.localDb.forEach(item => {
 
             if (item.id === id) {
@@ -37,12 +42,25 @@ class localDbClass{
         });
         this.save();
     }
-    
-    static deleteRecord(id){
-        this.localDb=this.localDb.filter( x=> x.id!==id)
+
+    static deleteRecord(id) {
+        this.localDb = this.localDb.filter(x => x.id !== id)
     }
 
-    
+    static makeSent(id) {
+        this.localDb.forEach(item => {
+
+            if (item.id === parseInt(id)) {
+                console.log("Record sent from localDb")
+                item.isSent = true
+            }
+
+        });
+        this.save();
+
+    }
+
+
 }
 
 
@@ -58,7 +76,7 @@ function createNewTask() {
     let newTaskText = document.getElementById("newTaskText").value;
     let whenTime = document.getElementById("whenTime").value;
 
-    if( newTaskText.length <=2 || whenTime.length===0){
+    if (newTaskText.length <= 2 || whenTime.length === 0) {
         alert("Please fill both Notes and time before saving.")
     }
 
@@ -135,7 +153,7 @@ if (todayBtn !== null) {
 
 
     document.addEventListener("keyup", function (event) {
-        if (event.key  === 13) {
+        if (event.key === 13) {
             createNewTask();
         }
     });
@@ -153,25 +171,28 @@ function fillScheduled() {
         tableTarget.children[1].innerHTML = "";
 
         localDb.forEach(item => {
-           
+
             if (item.isSent === false) {
                 let newRec = preparedRecord.cloneNode(true);
                 newRec.children[3].classList.remove("d-none")
                 newRec.children[0].innerHTML = item.id;
                 newRec.children[1].innerHTML = item.reminderNote;
                 newRec.children[2].innerHTML = new Date(item.remindingTime).toLocaleString();
-                if( item.isDismissed ){
+                if (item.isDismissed) {
                     console.log(item)
-                    
-                    newRec.children[3].children[0].children[1].innerText="Unsnooze"; 
 
+                    newRec.children[3].children[0].children[1].innerText = "Unsnooze";
+
+                }
+                else {
+                    newRec.children[3].children[0].children[1].innerText = "Snooze";
                 }
                 tableTarget.children[1].append(newRec)
 
-               
-                
 
-                
+
+
+
             }
 
         });
@@ -242,55 +263,67 @@ function validateTimers() {
 
 
 
- function checkTimeAndSendNotif( ) {
+function checkTimeAndSendNotif() {
 
-    var currentTs = new Date().getTime();  
+    var currentTs = new Date().getTime();
     var latestRecord = getUpcomingRecord()
-    if (latestRecord.length>0){
-        latestRecord =latestRecord[0]
-    
+    if (latestRecord.length > 0) {
+        latestRecord = latestRecord[0]
 
-    var timeLeft = new Date(latestRecord.remindingTime).getTime() - currentTs;
-   
-    // console.s(timeLeft/1000/60);
 
-    if (timeLeft>=0 && timeLeft <=1000 ) {
-        if (confirm(latestRecord.reminderNote)) {
-              playafterAsecond();
-            alert("Notification dismissed");
-            console.log("Notif sent")
-    
+        var timeLeft = new Date(latestRecord.remindingTime).getTime() - currentTs;
+
+        // console.s(timeLeft/1000/60);
+
+        if (timeLeft >= 0 && timeLeft <= 1000) {
+            if (confirm(latestRecord.reminderNote)) {
+                playafterAsecond();
+                alert("Notification dismissed");
+                console.log("Notif sent")
+
+            }
         }
     }
-}
-;
+    ;
 
 }
 
-// // setTimeout(sendNotifcation,timeLeft,latestRecord)
-// setInterval(checkTimeAndSendNotif,500); /asdasdasd
-// setInterval(fillCompletedTasks, 1000, false);
-// setInterval(fillScheduled, 1000, false);
-// setInterval(validateTimers, 1000);
- 
+// setTimeout(sendNotifcation,timeLeft,latestRecord)
+// setInterval(checkTimeAndSendNotif,500);  
+setInterval(fillCompletedTasks, 1000, false);
+setInterval(fillScheduled, 1000, false);
+setInterval(validateTimers, 1000);
+
+function deleteHandler(el) {
+    let id = el.parentElement.parentElement.children[0].innerText;
+    console.log(id);
+    localDbClass.load()
+    localDbClass.deleteRecord(parseInt(id));
+    if (confirm("Are you sure to delete the reminder?")) {
+        localDbClass.save();
+    }
+
+
+
+}
 
 function snoozeHandler(el) {
     let id = el.parentElement.parentElement.children[0].innerText;
-     
+
     console.log(id);
-    if (el.innerHTML.includes("Unsnooze") ) {
+    if (el.innerHTML.includes("Unsnooze")) {
 
         el.innerHTML = el.innerHTML.replace("Unsnooze", "Snooze")
 
         localDbClass.load()
-        localDbClass.unmarkDismissed(parseInt(id)); 
+        localDbClass.unmarkDismissed(parseInt(id));
 
     }
     else {
         el.innerHTML = el.innerHTML.replace("Snooze", "Unsnooze")
         localDbClass.load()
         localDbClass.markDismissed(parseInt(id));
-         
+
     }
 
 
